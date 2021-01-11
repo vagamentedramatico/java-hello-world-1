10a9938fa173e819c15beb4e61364fe834509c4c
vagamentedramatico <miguel.murak@gmail.com>
2021-01-11 12:55:21 +0000

    Adicionado git ignore (me)

--- src/pt/serro/test/Main.java
+++ src/pt/serro/test/Main.java
@@ -3,6 +3,6 @@ package pt.serro.test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-	System.out.println("ChChChChanges!");
+	    System.out.println("ChChChChanges!");
     }
 }
